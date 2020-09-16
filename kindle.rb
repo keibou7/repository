c1fe2ce2dbe3ea4f698919e7b3a3173c174dfc22
keibou7 <keisuke.takahashi@di-v.co.jp>
2020-09-16 17:21:09 +0900

    Update kindle.rb

--- kindle.rb
+++ kindle.rb
@@ -5,40 +5,35 @@
 class BookReader
   def initialize
     @books = [
-      { name: '7つの習慣' , review: book_review },
-      { name: '嫌われる勇気' , review: book_review },
-      { name: 'イシューからはじめよ' , review: book_review }
+      { name: '7つの習慣' , review: '書評' },
+      { name: '嫌われる勇気' , review: '書評'  },
+      { name: 'イシューからはじめよ' , review: '書評' }
     ]
   end
 
   def select_book_review
-    choose_book
-    puts '本を読む場合は0、書評を読む場合は1を選択してください'
-    select_num = gets.to_i
-    while true do
-      if select_num == 0
-        puts "#{book[:name]}を読みます"
-        book[:name]
-        break
-      elsif select_num == 1
-        puts "#{book[:name]}の書評を読みます"
-        book[:review]
-        break
-      else
-        puts '本を読む場合は0、書評を読む場合は1を選択してください'
-      end
-    end
-  end
-
-  def choose_book
     puts '気になる本を以下から選んでください'
     @books.each_with_index do |book, i|
       puts "[#{i}] 書名：#{book[:name]}"
     end
     selected = @books[gets.to_i]
     p "#{selected[:name]}が選択されました"
-    selected
+    puts '本を読む場合は0、書評を読む場合は1を選択してください'
+    select_num = gets.to_i
+    while true do
+      if select_num == 0
+        puts "#{selected[:name]}を読みます"
+        break
+      elsif select_num == 1
+        puts "#{selected[:name]}の書評を読みます"
+        break
+      else
+        puts '本を読む場合は0、書評を読む場合は1を選択してください'
+        select_num = gets.to_i
+      end
+    end
   end
+
 end
 
 bookreader = BookReader.new
