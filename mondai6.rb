214f07eaa5fef89b75faee33a42c8aae9d0143aa
Yusuke Yamane <yusukeyamane01@gmail.com>
2020-10-02 16:58:05 +0900

    Update mondai6.rb

--- mondai6.rb
+++ mondai6.rb
@@ -9,38 +9,40 @@
 def draw_lottery
   draw_number = input_shopping_amount
   lottery = [ 1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ]
-  lottery.shuffle!
-  first_prize = lottery[0]
-  second_prize = lottery[1]
-  third_prize = lottery[2]
+  prize_nums = pick_prize_number(lottery)
+
   while draw_number > 0
     puts '1〜10の中から好きな数字を入力してください'
-    array = [ 1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ]
-    puts array.join(" , ")
+    puts lottery.join(" , ")
     target = gets.to_i
-    if first_prize == target
+
+    if target == prize_nums[0]
       puts "おめでとうございます！#{target}は1等です！\n3000円分の商品券です！"
-    elsif second_prize == target
+    elsif target == prize_nums[1]
       puts "惜しかった！#{target}は2等です！\n1000円分の商品券です！"
-    elsif third_prize == target
+    elsif target == prize_nums[2]
       puts "もうちょっとでした！#{target}は3等です！\n500円分の商品券です！"
     else
       puts '残念！ハズレです！'
     end
+
     draw_number -= 1
-    if draw_number == 0
-      puts 'くじ引きは以上となります'
-    else
-      puts "残り#{draw_number}回です"
-    end
+    puts "残り#{draw_number}回です"
   end
+
+  puts 'くじ引きは以上となります'
 end
+
+def pick_prize_number(array)
+  shuffled = array.shuffle
+  [shuffled[0], shuffled[1], shuffled[2]]
+end
+
 def input_shopping_amount
   puts "今日はいくらお買い物をしましたか？"
   shopping_amount = gets.to_i
   puts "#{(shopping_amount / 1000).floor}回くじが引けます"
-  draw_number = (shopping_amount / 1000).floor
-  return draw_number
+  (shopping_amount / 1000).floor
 end
 
 draw_lottery
